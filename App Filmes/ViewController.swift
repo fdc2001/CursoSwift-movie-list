8d91d52bfa4fbc97d88e7a3756eb1467bec0ab94
Filipe Clemente <5507892-FilipeClemente@users.noreply.gitlab.com>
2022-06-08 21:37:31 +0100

    Initial Commit

---
+++ App Filmes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Filmes
+//
+//  Created by Filipe Clemente on 08/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
